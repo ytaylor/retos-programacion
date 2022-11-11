4e9c9419d42bdc8e94f1eb19b83135015ad04031
Yanelis Serrano <yaneliserranotaylor@gmail.com>
2022-11-11 10:43:10 +0100

    Add other solution to boomerangs

--- bumeranes/js/main.js
+++ bumeranes/js/main.js
@@ -11,22 +11,32 @@ en el que el primero y el último son iguales, y el segundo es diferente. Por ej
 
 
 'use strict'
-const array = [2, 1, 2, 3, 3, 4, 2, 4]; 
+const array = [2, 1, 2, 3, 3, 4, 2, 4];
 
-function countTotalBumera(array){
-    let countTotal = 0; 
-   //recorrer el array y hacer una subarray de tres en tres y comprobar cada elemento del array
+function countTotalBumeran(array) {
+    let countTotal = 0;
+    //recorrer el array y hacer una subarray de tres en tres y comprobar cada elemento del array
 
-    for (let index = 0; index < array.length; index++) 
-    {
-     const newArray = array.slice(index,index+3); // solo me quedo desde la posicion que estoy haste tres elemento más
-        if(newArray[0]=== newArray[2] &&  newArray[0] != newArray[1] )
-        {
+    if (array.length < 3) return countTotal;
+
+    for (let index = 0; index < array.length; index++) {
+        const newArray = array.slice(index, index + 3); // solo me quedo desde la posicion que estoy haste tres elemento más
+        if (newArray[0] === newArray[2] && newArray[0] != newArray[1]) {
             console.log(newArray);
-            countTotal++; 
+            countTotal++;
         }
-   }
-   return countTotal; 
+
+        //other solution
+        // if (array[index] === array[index + 2] && array[index] != array[index + 1]) {
+        // }
+    }
+    return countTotal;
 }
 
-console.log(`La cantidad total de bumeran es ${countTotalBumera(array)}`);
+console.log(`La cantidad total de bumeran es ${countTotalBumeran(array)}`);
+
+//other examples
+console.log(`La cantidad total de bumeran es ${countTotalBumeran([2, 1, 2, 1, 2])}`);
+console.log(`La cantidad total de bumeran es ${countTotalBumeran([1, 2, 3, 4, 5])}`);
+console.log(`La cantidad total de bumeran es ${countTotalBumeran([1, 2, 3, 4, 5])}`);
+console.log(`La cantidad total de bumeran es ${countTotalBumeran([1, 2, 3, 4, 5])}`);
